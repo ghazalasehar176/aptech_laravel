98da896ac48e86b862f4d241e41fb9d063d2ab08
ghazalasehar176 <aptechsrj176@gmail.com>
2025-06-13 23:40:13 +0500

    hello laravel

---
+++ class_1/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home/' , function() {
+return view ('home');
+});
+
+Route::get('about/' , function() {
+return view ('welcome');
+});
+Route::get('abcd/'  , function () {
+echo "basic laravel routes ";
+});
+
+
+//short form to open a page
+Route::view('/home'  , 'home');
+
+
+// Route::view('(home/{name})' , function($name){
+//   return view('home' , ['name'=>$name]);
+// });
